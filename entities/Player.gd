fbf235def896e86108dec5eec83e226e3dcd3afe
Robstuh <Robstuh@users.noreply.github.com>
2026-04-21 22:39:08 +0100

    Update Player.gd

--- entities/Player.gd
+++ entities/Player.gd
@@ -3,6 +3,13 @@ extends Actor
 
 @export var move_speed: float = 180.0
 
+func _ready() -> void:
+    GameState.current_player = self
+    connect("leveled_up", Callable(self, "_on_leveled_up"))
+
+func _on_leveled_up(new_level):
+    print("Leveled up to level ", new_level, "!")
+
 func _physics_process(delta: float) -> void:
     var input_vec = Vector2.ZERO
     input_vec.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
