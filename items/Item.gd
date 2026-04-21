f764dd152930e0010a4260539b1fbecf3c40c761
Robstuh <Robstuh@users.noreply.github.com>
2026-04-21 22:39:11 +0100

    Create Item.gd

---
+++ items/Item.gd
@@ -0,0 +1,9 @@
+class_name Item
+extends Resource
+
+@export var id: String = ""
+@export var name: String = ""
+@export var description: String = ""
+@export var icon: Texture2D
+@export var stackable: bool = true
+@export var max_stack: int = 99
